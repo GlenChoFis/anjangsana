b6be904b64fc368cd296d1476807ee3092100a4a
GlenChoFis <glenchoirulfais@gmail.com>
2022-09-29 15:32:54 +0700

    bret

--- resources/views/generic.blade.php
+++ resources/views/generic.blade.php
@@ -50,17 +50,48 @@
 									facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor<br />
 									amet nullam sed etiam veroeros.</p> -->
 								</header>
+								<div>
+								<p><h2> Mengenal Jalur Pendakian Gunung Lawu Via Jogorogo, Laku Tirakat dan Khusus Profesional</h2></p>
+								</div>
 								<div class="image main">
-								<img src="https://cdn-1.timesmedia.co.id/images/2022/09/27/Pendakian-Gunung-Lawu.jpg" alt="" /></div>
-								<p>TIMESINDONESIA, NGAWI </p>
-								<p>Gunung Lawu sudah masyhur menjadi jujugan para pecinta kegiatan pendakian. Para pendaki umumnya memilih lewat jalur pendakian di Kabupaten Magetan, atau di Karanganyar, Jawa Tengah untuk mencapai puncak. Namun tahukah, di Kabupaten Ngawi juga terdapat jalur pendakian Gunung Lawu. Jalur pendakian Gunung Lawu via Ngawi dijuluki dengan sebutan Wukir Bayi. Pos pemberangkatan ada di Desa Kletekan, Kecamatan Jogorogo, masuk kawasan hutan perhutani. Tidak sedikit juga yang menyebutnya jalur pendakian Gunung Lawu via Jogorogo. Ketua Karang Taruna desa setempat, Rolanda Devi Cahyana mengatakan, jalur pendakian Wukir Bayi termasuk jalur pendakian Gunung Lawu yang termasuk tua. Jalur ini sering digunakan untuk laku tirakat.</p>
+								<figure>
+								<img src="https://cdn-1.timesmedia.co.id/images/2022/09/27/Pendakian-Gunung-Lawu.jpg" alt="" />
+								<figcaption>Plang penunjuk arah jalur pendakian via jogorogo. (Miftakul/TIMES INDONESIA)</figcaption>
+								</figure>
+								<p>
+									<h6>Pewarta: Muhammad Miftakul Falakh (MG-425) | Editor: Deasy Mayasari</h6>
+								</p>
+								</div>
+								<p><b>TIMESINDONESIA, NGAWI-</b>Gunung Lawu sudah masyhur menjadi jujugan para pecinta kegiatan pendakian. Para pendaki umumnya memilih lewat jalur pendakian di Kabupaten Magetan, atau di Karanganyar, Jawa Tengah untuk mencapai puncak. Namun tahukah, di Kabupaten Ngawi juga terdapat <b>jalur pendakian Gunung Lawu</b>. Jalur pendakian Gunung Lawu via Ngawi dijuluki dengan sebutan Wukir Bayi. Pos pemberangkatan ada di Desa Kletekan, Kecamatan Jogorogo, masuk kawasan hutan perhutani. Tidak sedikit juga yang menyebutnya jalur pendakian Gunung Lawu via Jogorogo. Ketua Karang Taruna desa setempat, Rolanda Devi Cahyana mengatakan, jalur pendakian Wukir Bayi termasuk jalur pendakian Gunung Lawu yang termasuk tua. Jalur ini sering digunakan untuk laku tirakat.</p>
 								<p>"Bisa dikatakan jalur pendakian gunung Lawu via Jogorogo ini termasuk tua, jalur untuk tirakat," kata Yana kepada TIMES Indonesia, Selasa (27/9/2022).</p>
 								<p>Khusus untuk laku tirakat, Yana Menjelaskan, masyarakat sekitar memiliki kebiasaan untuk mendaki gunung Lawu setiap tahun sekali. Pelaksanaannya saban bulan Suro. Masyarakat Kletekan biasa mendaki saat malam Jumat Legi, pada bulan tersebut.</p>
 								<p>"Memang dipakai masyarakat Jawa untuk jalur tirakat. Tidak hanya masyarakat sekitar, tapi juga berbagai wilayah. Setiap malam Jumat Legi pada bulan Suro juga rutin dilakukan upacara Wedhus Kendhit," kata Yana.</p>
 								<p>Yana mengatakan, jalur pendakian via Jogorogo dipercaya sebagai pintu masuk gunung Lawu. Hal itu juga dipercaya para pendaki yang memang memiliki hajat laku tirakat dengan naik gunung Lawu via jalur Jogorogo.</p>
 								<p>"Masyarakat percaya, pintu masuk gunung Lawu ada di jalur pendakian via Jogorogo ini," kata Yana.</p>
 								<p>Meskipun dikenal sebagai jalur pendakian gunung Lawu khusus untuk tirakat, namun tidak banyak pantangan yang neko-neko. Hanya saja, para pendaki diwanti-wanti untuk selalu menjaga omongan, dan tidak boleh congkak selama mendaki.</P>
-								<p>"Juga ada tradisi, setiap pendaki yang akan naik via jalur ini untuk membawa sepasang kupat lepet. Dua jenis makanan ini dibawa selama naik dan turun gunung Lawu," kata Yana.</p>
+								<p> "Juga ada tradisi, setiap pendaki yang akan naik via jalur ini untuk membawa sepasang kupat lepet. Dua jenis makanan ini dibawa selama naik dan turun gunung Lawu," kata Yana.</p>
+								<p><b>Trek Panjang, Jalur Ekstrim Khusus untuk Pendaki Profesional</b></p>
+								<div class="image main">
+								<figure>
+								<img src="https://cdn-1.timesmedia.co.id/images/2022/09/27/Pendakian-Gunung-Lawu.jpg" alt="" />
+								<figcaption>Plang pos 1 Wukir Bayi, di jalur pendakian via Jogorogo. (Foto: Miftakul/TIMES Indonesia)</figcaption>
+								</figure>
+								</div>
+								<p>Jalur pendakian gunung Lawu via Jogorogo memiliki total panjang trek 15 kilometer, dari titik awal pemberangkatan hingga puncak. Untuk mencapai puncak, dapat ditempuh dengan waktu kurang lebih 12 jam perjalanan.</p>
+								<p>Dikatakan Yana, jalur pendakian gunung Lawu via Jogorogo memiliki 8 pos pendakian. Secara berurutan dari pos 1 yaitu Wukir Bayi, Ndoro Wati, Ngudal, Ondo Rante, Pendem, Cemoro Lawang, Bulak Peperangan, Pasar Dieng, dan Puncak.</p>
+								<p>"Saat ini kita baru memiliki 3 pos dengan bangunan semi permanen. Kita masih punya pekerjaan rumah untuk membangun 5 pos lainnya," kata Yana.</p>
+								<p>Yana mengatakan, untuk topografi jalur pendakian via Jogorogo, pada pos 1 hingga pos 4 jalur yang dilintasi relatif landai. Kemudian setelahnya, saat memasuki pos Ondo Rante, pendaki akan melintasi jalur yang ekstrim. Sudut kemiringan jalur antara 60 sampai dengan 70 derajat.</p>
+								<p>"Sampai dengan bulak peperangan hingga puncak jalur pendakian relatif landai lagi," katanya.</p>
+								<p>Di samping itu, hutan sepanjang trek yang akan dilewati pendaki masih perawan. Masih alami dan lebat. Mengingat hal itu, pihak pengelola jalur pendakian membatasi hanya untuk pendaki yang profesional.</p>
+								<p>"Kita masih batasi untuk pendaki yang sudah profesional. Untuk pemula kita arahkan ke jalur lain. Sebab, treknya panjang, hutan masih lebat kita takutkan nanti tersesat," katanya.</p>
+								<p>Dalam menjaga dari hal-hal yang tidak diinginkan, karang taruna setempat juga telah membentuk tim SAR. Tim SAR yang sudah terlatih, disiapkan untuk memberikan pertolongan bagi pendaki yang mengalami kesulitan saat melintas di jalur pendakian tersebut.</p>
+								<p>"Sebelum mendaki kita pastikan, para pendaki siap secara perbekalan, perlengkapan, serta pengalaman mendakinya," ujar Yana.</p>
+								<p>Terlepas dari jalur pendakian yang ekstrim, Yana menyebut, para pendaki akan disuguhkan pemandangan alam yang eksotis. Selain itu, sejumlah keanekaragaman hayati juga masih asri di jalur pendakian via Jogorogo.</p>
+								<p>"Pemandangannya indah, kemudian juga kekayaan hayati seperti tanaman, hingga fauna juga masih banyak. Seperti babi hutan, lutung, rusa, berbagai macam jenis burung masih sering ditemui," kata Yana.</p>
+								<p>Potensi jalur pendakian gunung Lawu via Jogorogo rupanya masih terkatung-katung secara kejelasan legalitas izin. Meskipun karang taruna setempat sudah siap untuk mengelola jalur pendakian tersebut.</p>
+								<p>"Sampai saat ini belum ada kejelasan lagi terkait izin. Problem kita di izin yang masih mandek dan juga pendanaan. Padahal kita sudah siap, penunjuk jalan juga sudah ada dan lainnya," ujar Yana.</p>
+								<p>Karang taruna setempat sudah berupaya mengajukan izin kepada Kementerian Lingkungan Hidup dan Kehutanan sampai izin Kehutanan sosial. Pihak Perhutani sudah memberikan lampu hijau, untuk memberikan fasilitasi bagi para pendaki.</p>
+								<p>"Kita masih menunggu selama 6 bulan. Semoga segera terealisasi dan kita bisa mengelola jalur pendakian Gunung Lawu via Jogorogo ini," harap Rolanda Devi Cahyana, ketua karang taruna di desa Kletekan, Kecamatan Jogorogo, Kabupaten Ngawi.</p>
 							</section>
 
 					</div>
