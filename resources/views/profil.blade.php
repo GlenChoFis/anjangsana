7e7c5139998224a671a6464c36320b7021b7e9e8
GlenChoFis <glenchoirulfais@gmail.com>
2022-10-09 22:28:42 +0700

    mek

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,7 +1,7 @@
 <div class="container rounded bg-white mt-5">
         <div class="row">
             <div class="col-md-4 border-right">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" src="https://akcdn.detik.net.id/community/media/visual/2022/10/06/erling-haaland_169.jpeg?w=700&q=90" width="90"><span class="font-weight-bold">John Doe</span><span class="text-black-50">john_doe12@bbb.com</span><span>United States</span></div>
+                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" img src="fotoprofil.jpeg" alt="" width="90"><span class="font-weight-bold">John Doe</span><span class="text-black-50">john_doe12@bbb.com</span><span>United States</span></div>
             </div>
             <link rel="stylesheet" href="assets/css/profil.css">
             <div class="col-md-8">
@@ -13,21 +13,21 @@
                         <h6 class="text-right">Edit Profile</h6>
                     </div>
                     <div class="row mt-2">
-                        <div class="col-md-6"><input type="text" class="form-control" placeholder="first name" value="John"></div>
-                        <div class="col-md-6"><input type="text" class="form-control" value="Doe" placeholder="Doe"></div>
+                        <div class="col-md-6"><input type="text" class="form-control" placeholder="first name" value="Glen "></div>
+                        <div class="col-md-6"><input type="text" class="form-control" value="ChoFis" placeholder="ChoFis"></div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-6"><input type="text" class="form-control" placeholder="Email" value="john_doe12@bbb.com"></div>
-                        <div class="col-md-6"><input type="text" class="form-control" value="+19685969668" placeholder="Phone number"></div>
+                        <div class="col-md-6"><input type="text" class="form-control" placeholder="Email" value="glenchofis@yahoo.com"></div>
+                        <div class="col-md-6"><input type="text" class="form-control" value="081284823491" placeholder="Phone number"></div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-md-6"><input type="text" class="form-control" placeholder="address" value="D-113, right avenue block, CA,USA"></div>
-                        <div class="col-md-6"><input type="text" class="form-control" value="USA" placeholder="Country"></div>
+                        <div class="col-md-6"><input type="text" class="form-control" placeholder="address" value="Jl.Kertajaya no 5"></div>
+                        <div class="col-md-6"><input type="text" class="form-control" value="INDONESIA" placeholder="Country"></div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col-md-6"><input type="text" class="form-control" placeholder="Bank Name" value="Bank of America"></div>
+                    <!-- <div class="row mt-3">
+                        <div class="col-md-6"><input type="text" class="form-control" placeholder="Bank Name" value="BNI"></div>
                         <div class="col-md-6"><input type="text" class="form-control" value="043958409584095" placeholder="Account Number"></div>
-                    </div>
+                    </div> -->
                     <div class="mt-5 text-right"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
                 </div>
             </div>
