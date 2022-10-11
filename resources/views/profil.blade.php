209fea49be2b5d093591bb41eff96d31baa95433
arifbucholi <muhammad.arif.ma23@gmail.com>
2022-10-11 10:16:58 +0700

    mboh kah

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,16 +1,17 @@
 <div class="container rounded bg-white mt-5">
         <div class="row">
             <div class="col-md-4 border-right">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" src="pic01.png" alt="" width="90"><span class="font-weight-bold">John Doe</span><span class="text-black-50">john_doe12@bbb.com</span><span>United States</span></div>
+                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" src="https://lh6.googleusercontent.com/proxy/ZW1YAkPTczms6ZdPzQDf9zRzeCCadEsKMIIEDYyD3HHAXGbF2wrcF3DxDwJDZFRU3jFDuYEn5x1hik3jnxxaucn8LK9E9ZUBpc-stufX2P08HIL6wMdPAzdUmGqSg0_Cj0yQdLmco8MQtUWpYqssNbVRiksgjLsvuD7Vc49eSeSd-6qgX6aMEqxfLQAfc-g9KX1cs5LCtngBYT0JE9nd4s6eIONsrGfN3D-n=w1200-h630-p-k-no-nu" alt="" width="90"><span class="font-weight-bold">Glen</span><span class="text-black-50">glenchofis@yahoo.com</span><span>Indonesia</span></div>
             </div>
             <link rel="stylesheet" href="assets/css/profil.css">
             <div class="col-md-8">
                 <div class="p-3 py-5">
                     <div class="d-flex justify-content-between align-items-center mb-3">
-                        <div class="d-flex flex-row align-items-center back"><i class="fa fa-long-arrow-left mr-1 mb-1"></i>
-                            <h6>Back to home</h6>
+                        <div class="d-flex flex-row align-items-center back">
+                            <h6><a href="/menuutama">Menu Utama</a></h6>
                         </div>
-                        <h6 class="text-right">Edit Profile</h6>
+                        <h5 class="text-right">Edit Profile</h5>
+                        
                     </div>
                     <div class="row mt-2">
                         <div class="col-md-6"><input type="text" class="form-control" placeholder="first name" value="Glen "></div>
